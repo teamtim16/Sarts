dc2b4704f363daa9cc4460c0e0410731c68b782e
Myounghoon Lee <57764124+teamtim16@users.noreply.github.com>
2022-10-13 17:06:46 +0900

    js/temporal edit

--- sketch.js
+++ sketch.js
@@ -1,56 +1,52 @@
-let cx, cy;
-let secondsRadius;
-let minutesRadius;
-let hoursRadius;
-let clockDiameter;
+let b;
+let bs = [];
 
 function setup() {
-  createCanvas(720, 400);
-  stroke(255);
-
-  let radius = min(width, height) / 2;
-  secondsRadius = radius * 0.71;
-  minutesRadius = radius * 0.6;
-  hoursRadius = radius * 0.5;
-  clockDiameter = radius * 1.7;
-
-  cx = width / 2;
-  cy = height / 2;
+  createCanvas(400, 200);
+  
+  // b = new Walker();
+  for (let i=0; i<100; i++) {
+    let c = color(random(50, 200));
+    let walker = new Walker(c);
+    bs.push(walker);
+  }
 }
+
 
 function draw() {
-  background(230);
-
-  // 시계 배경 그리기
-  noStroke();
-  fill(244, 122, 158);
-  ellipse(cx, cy, clockDiameter + 25, clockDiameter + 25);
-  fill(237, 34, 93);
-  ellipse(cx, cy, clockDiameter, clockDiameter);
-
-  // sin()과 cos()의 각도는 3시 정각에서 시작;
-  // HALF_PI를 뺄셈하여 상단에서부터 시작하도록 설정
-  let s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
-  let m = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
-  let h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
-
-  // 시계침들 그리기
-  stroke(255);
-  strokeWeight(1);
-  line(cx, cy, cx + cos(s) * secondsRadius, cy + sin(s) * secondsRadius);
-  strokeWeight(2);
-  line(cx, cy, cx + cos(m) * minutesRadius, cy + sin(m) * minutesRadius);
-  strokeWeight(4);
-  line(cx, cy, cx + cos(h) * hoursRadius, cy + sin(h) * hoursRadius);
-
-  // 분침 그리기
-  strokeWeight(2);
-  beginShape(POINTS);
-  for (let a = 0; a < 360; a += 6) {
-    let angle = radians(a);
-    let x = cx + cos(angle) * secondsRadius;
-    let y = cy + sin(angle) * secondsRadius;
-    vertex(x, y);
+  background(220, 5);
+  // b.walk();
+  // b.show();
+  
+  for (let i=0; i<bs.length; i++) {
+    bs[i].walk();
+    bs[i].show();
   }
-  endShape();
 }
+
+
+class Walker {
+  constructor(aColor) {
+    // this.x = width/2;
+    // this.y = height/2;
+    this.pos = createVector(width/2, height/2);
+    this.w = 1;
+    this.e = 3;
+    this.c = aColor;
+  }
+  
+  walk() {
+    // this.x += random(-this.e, this.e);
+    // this.y += random(-this.e, this.e);
+    
+    this.vel = createVector(random(-this.e, this.e), random(-this.e, this.e));
+    this.pos.add(this.vel);
+  }
+  
+  show() {
+    noStroke();
+    fill(this.c);
+    ellipse(this.pos.x, this.pos.y, this.w);
+  }
+  
+} 
